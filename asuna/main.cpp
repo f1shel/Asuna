18d37d1ede4f00b9d343dcb662c767edfc5c9977
xiangfeng <f1shel@outlook.com>
2022-04-02 18:31:07 +0800

    nvvk test

--- asuna/main.cpp
+++ asuna/main.cpp
@@ -1,6 +1,15 @@
+#include <nvvk/context_vk.hpp>
+
 #include <iostream>
 using namespace std;
 int main() {
     cout << "Hello, Asuna!" << endl;
+
+    // Create the Vulkan context, consisting of an instance, device, physical device, and queues.
+    nvvk::ContextCreateInfo deviceInfo;  // One can modify this to load different extensions or pick the Vulkan core version
+    nvvk::Context           context;     // Encapsulates device state in a single object
+    context.init(deviceInfo);            // Initialize the context
+    context.deinit();                    // Don't forget to clean up at the end of the program!
+
     return 0;
 }
