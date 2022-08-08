a6c615c1f99819aa38b627858b3c15e61d73f476
xiangfeng <f1shel@outlook.com>
2022-08-08 11:42:32 +0800

    fix: forget to output channel 6

--- src/tracer/tracer.cpp
+++ src/tracer/tracer.cpp
@@ -245,7 +245,7 @@ void Tracer::runOffline() {
     // sprintf(outputName, "%s_shot_%04d_channel_1.exr",
     // m_tis.outputname.c_str(), shotId); saveBufferToImage(pixelBuffer,
     // outputName, 1);
-    for (int channelId = 1; channelId < 6; channelId++) {
+    for (int channelId = 1; channelId < 7; channelId++) {
       sprintf(outputName, "%s_shot_%04d_channel_%04d.exr",
               m_tis.outputname.c_str(), shotId, channelId);
       saveBufferToImage(pixelBuffer, outputName, channelId);
